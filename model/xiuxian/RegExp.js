3518c652b5b46e7a03cee10d37c09a191c7e983d
MozuGit <mozujieshuo@hotmail.com>
2026-06-25 02:58:45 +0000

    细节优化

--- model/xiuxian/RegExp.js
+++ model/xiuxian/RegExp.js
@@ -3,7 +3,7 @@ import { Config } from "./tool/Config/Config.js"
 const prefix = Config.setting.forceSharp ? '^#' : '^#?'
 
 const patterns = [
-  /(修炼|开采|修仙签到|修仙个人信息|突破|渡劫)/,
+  /(修炼|开采|修仙签到|修仙个人信息|突破)/,
   /(确认)?(开始|结束)闭关/,
   /(切磋|查询修仙者|加入宗门)\s*\d*/,
   /(修为|灵石|战力|闭关)榜/,
