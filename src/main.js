85a039dfb74165695bd0ae62d83b0234a7dcb700
MozuGit <mozujieshuo@hotmail.com>
2026-08-07 02:57:43 +0000

    [新增] 面版添加favicon.ico图标

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
+import './favicon.ico'
 const app = createApp(App)
 app.use(router)
 app.use(Antd)
